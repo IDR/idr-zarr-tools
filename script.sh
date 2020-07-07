e8274865a9129672dae185015768231b0ee84d92
jmoore <josh@glencoesoftware.com>
2020-07-07 09:50:19 +0100

    Last upload pre-masks

--- script.sh
+++ script.sh
@@ -7,9 +7,18 @@ set -e
 set -u
 set -x
 
+IMAGE=$1; shift
 INPUT=$1; shift
 OUTPUT=$1; shift
 
-echo "Converting $INPUT to /out/$OUTPUT..."
-time $EXE $INPUT /out/$OUTPUT --file_type=zarr --dimension-order=XYZCT
-ls /out/$OUTPUT
+echo "Converting $INPUT to /$OUTPUT..."
+time $EXE "$INPUT" "/$OUTPUT" --file_type=zarr --dimension-order=XYZCT
+ls "/$OUTPUT"
+
+# TBD Move first series to the image name
+mv /$OUTPUT/data.zarr/0 /v0.1/${IMAGE}.zarr
+rm /$OUTPUT/data.zarr/.zattrs
+rm /$OUTPUT/data.zarr/.zgroup
+rmdir /$OUTPUT/data.zarr
+rm /$OUTPUT/METADATA.ome.xml
+rmdir /$OUTPUT/
