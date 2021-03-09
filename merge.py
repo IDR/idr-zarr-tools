cfe3fb244599bc84f79795e7654b9d0fac3006d3
Sebastien Besson <seb.besson@gmail.com>
2021-03-09 22:25:38 +0000

    Add dry-run option

--- merge.py
+++ merge.py
@@ -5,7 +5,7 @@ import json
 import argparse
 
 
-def update_zattrs(zarr_dir, rendering_json=None):
+def update_zattrs(zarr_dir, rendering_json=None, dry_run=False):
     rv = dict()
     if os.path.exists(f"{zarr_dir}/.zattrs"):
         with open(f"{zarr_dir}/.zattrs") as o:
@@ -35,6 +35,8 @@ def update_zattrs(zarr_dir, rendering_json=None):
     rv["omero"]["rdefs"].pop("invertAxis", None)
     rv["omero"]["rdefs"].pop("projection", None)
 
+    if args.dry_run:
+        print(f"Updating {zarr_dir}/.zattrs")
     with open(f"{zarr_dir}/.zattrs", "w") as o:
         o.write(json.dumps(rv, indent=4, sort_keys=True))
 
@@ -67,6 +69,8 @@ if __name__ == "__main__":
     p.add_argument("dir", nargs="+", help="the Zarr directory")
     p.add_argument("rendering_json_file", nargs="?",
                    help="a JSON file containing the rendering settings")
+    p.add_argument("--dry-run", "-n", action='store_true',
+                   help="dry-run")
     p.add_argument("--recursive", "-r", action='store_true',
                    help="apply recursively to all Zarr sub-directories")
     args = p.parse_args()
@@ -74,4 +78,4 @@ if __name__ == "__main__":
     zarr_dirs = get_zarr_directories(args)
     rendering_json = get_rendering_json(args)
     for zarr_dir in zarr_dirs:
-        update_zattrs(zarr_dir, rendering_json=rendering_json)
+        update_zattrs(zarr_dir, rendering_json=rendering_json, dry_run=False)
